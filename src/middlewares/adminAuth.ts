ea5c09a6d4f6a30d07e7f6434142251a2dcdd159
@Robs_dagreat <robsrwibutso12@gmail.com>
2024-04-09 22:01:17 +0200

    added some authentication for admin and user

---
+++ src/middlewares/adminAuth.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, {JwtPayload} from 'jsonwebtoken';
+import { jwtSecretKey } from "../utils.js";
+
+
+const isAdmin = async (req: Request, res: Response, next: NextFunction): Promise<void>=>{
+    try{const token = req.headers.authorization?.split(' ')[1];
+
+    if(!token){
+        res.status(401).json({message: 'Unauthorized. Token missing!'});
+        return;
+    }
+
+    let decoded: JwtPayload;
+    
+    if(typeof token === 'string'){
+        decoded = jwt.verify(token, jwtSecretKey) as JwtPayload;
+    } else{
+        throw new Error ("Invalid token type");
+    }
+
+    if(decoded && decoded.isAdmin){
+        next();
+    }else{
+        res.status(403).json({message: 'Not authorized as admin'});
+    }
+
+    } catch(error){
+        console.error(error);
+        res.status(500).json({message: 'Server Error'});
+    }
+}
+
+export default isAdmin;
---
+++ src/middlewares/authenticate.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, {JwtPayload} from 'jsonwebtoken';
+import { jwtSecretKey } from "../utils.js";
+
+
+interface AuthenticatedRequest extends Request{
+    userId?: string;
+    username?: string;
+}
+
+
+const authenticateUser= async(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> =>{
+    try{
+
+        const token: string | undefined = req.headers.authorization?.split(' ')[1];
+
+        if(!token){
+            res.status(401).json({message: 'Authentication failed. Token missing!'});
+        }
+
+        let decoded : JwtPayload;
+
+          if(typeof token === 'string'){
+            decoded = jwt.verify(token, jwtSecretKey) as JwtPayload;
+        } else{
+            throw new Error('Invalid token type');
+        }
+
+        req.userId = decoded.userId;
+        req.username= decoded.name;
+
+     next();        
+    } catch(error){
+      console.error(error);
+      res.status(500).json({message: 'Server error'});
+    }
+}
+
+export  default authenticateUser;
