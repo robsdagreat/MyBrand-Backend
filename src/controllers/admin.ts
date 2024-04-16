c74ae0fe8d8eaeec0c7744d3968f8573ab9a2e54
@Robs_dagreat <robsrwibutso12@gmail.com>
2024-04-16 09:53:37 +0200

    added some changes

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -9,39 +9,28 @@ const adminLogin = async (req: Request, res: Response): Promise<void> => {
     try {
         const { email, password } = req.body;
 
-        
+
+
         if (!email || !password) {
             res.status(400).json({ message: 'Email and password are required' });
             return;
         }
 
-        
         if (email !== process.env.ADMIN || password !== process.env.ADMINPASS) {
             res.status(401).json({ message: 'Invalid credentials' });
             return;
         }
 
-        
-        const token = jwt.sign(
-            { email, isAdmin: true},
-            jwtSecretKey,
-        );
-
+        const token = jwt.sign({ email, isAdmin: true }, jwtSecretKey);
         const expiryDate = new Date(Date.now() + 1 * 24 * 60 * 60 * 1000);
 
-        res.cookie(
-            'jwt',
-            token,
-            {httpOnly: true, path: '/', expires: expiryDate},  
-        ),
-        res.status(200).json({ message: 'Welcome back Admin!', token });
-        return;
+        res.cookie('jwt', token, { httpOnly: true, path: '/', expires: expiryDate });
+        res.status(200).json({ message: 'Admin logged in successfully!' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Server Error' });
-        return;
     }
-}
+};
 
 
 
